52b01d861588ddfbcd33806b692bcdafaa331193
Samat Bekkulov <samat7089@gmail.com>
2022-05-01 15:40:43 +0300

    Initial commit

---
+++ Assets/Scripts/Ball/BallJumper.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(Rigidbody))]
+public class BallJumper : MonoBehaviour
+{
+    [SerializeField] private float _jumpForce;
+
+    private Rigidbody _rigidbody;
+
+    public event UnityAction LevelPassed;
+
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.TryGetComponent(out PlatformSegment platformSegment))
+        {
+            _rigidbody.velocity = Vector3.zero;
+            _rigidbody.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
+        }
+        else if(collision.gameObject.TryGetComponent(out FinishPlatform finishPlatform))
+        {
+            LevelPassed?.Invoke();
+        }
+    }
+}
---
+++ Assets/Scripts/InformLevelPassed.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class InformLevelPassed : MonoBehaviour
+{
+    [SerializeField] private BallJumper _ball;
+    [SerializeField] private TMP_Text _message;
+
+    private void Awake()
+    {
+        _message.text = "";
+    }
+
+    private void OnEnable()
+    {
+        _ball.LevelPassed += WriteLevelPassed;
+    }
+
+    private void OnDisable()
+    {
+        _ball.LevelPassed -= WriteLevelPassed;
+    }
+
+    public void WriteLevelPassed()
+    {
+        Debug.Log("Finish");
+        _message.text = "Вы прошли уровень!";
+    }
+}
